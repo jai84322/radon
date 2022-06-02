6afef2a242f9e022331ab4d5401422b36817a994
jai84322 <jai84322@gmail.com>
2022-06-02 18:37:11 +0530

    assignment/missingnumber

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,41 +1,16 @@
 const express = require('express');
-const myHelper = require('../util/helper')
-const underscore = require('underscore')
+const missing = require('../missingnumber/missingnumber')
+
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    myHelper.printDate()
-    myHelper.getCurrentMonth()
-    myHelper.getCohortData()
-    let firstElement = underscore.first(['Sabiha','Akash','Pritesh'])
-    console.log('The first element received from underscope function is '+firstElement)
-    res.send('My first ever api!')
+router.get('/prob1', function (req, res) {
+    res.send(missing.a());
 });
 
-router.get('/hello', function (req, res) {
-   
-    res.send('Hello there!')
-});
-
-router.get('/candidates', function(req, res){
-    console.log('Query paramters for this request are '+JSON.stringify(req.query))
-    let gender = req.query.gender
-    let state = req.query.state
-    let district = req.query.district
-    console.log('State is '+state)
-    console.log('Gender is '+gender)
-    console.log('District is '+district)
-    let candidates = ['Akash','Suman']
-    res.send(candidates)
-})
-
-router.get('/candidates/:canidatesName', function(req, res){
-    console.log('The request objects is '+ JSON.stringify(req.params))
-    console.log('Candidates name is '+req.params.canidatesName)
-    res.send('Done')
-})
-
+router.get('/prob2', function (req, res) {
+    res.send(missing.b());
+    });
 
 module.exports = router;
 // adding this comment for no reason
