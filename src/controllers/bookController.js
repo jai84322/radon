42dd7f82215eaf63d10dee83eef3f5705b5835cc
jai84322 <jai84322@gmail.com>
2022-06-06 10:44:39 +0530

    finally-done

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,13 +1,13 @@
-const userModel= require("../models/userModel")
+const bookModel= require("../models/bookModel")
 
 const createBook= async function (req, res) {
     let data= req.body
-    let savedData= await userModel.create(data)
+    let savedData= await bookModel.create(data)
     return res.send({msg: savedData})
 }
 
 const getBookData= async function (req, res) {
-    let allUsers= await userModel.find()
+    let allUsers= await bookModel.find()
     return res.send({msg: allUsers})
 }
 
