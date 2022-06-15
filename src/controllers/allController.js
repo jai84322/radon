c365103087632df66419fd5a09a092119936e6f3
jai84322 <jai84322@gmail.com>
2022-06-15 16:07:07 +0530

    assignment/auth-2

---
+++ src/controllers/allController.js
@@ -0,0 +1,62 @@
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/userModel");
+
+const createUser = async function (req, res) {
+  let data = req.body;
+  let savedData = await userModel.create(data);
+  return res.send({ msg: savedData });
+};
+
+const loginUser = async function (req, res) {
+  let userName = req.body.emailId;
+  let password = req.body.password;
+
+  let user = await userModel.findOne({ emailId: userName, password: password });
+  if (!user)
+    return res.send({
+      status: false,
+      msg: "username or the password is not corerct",
+    });
+
+
+  let token = jwt.sign(
+    {
+      userId: user._id.toString(),
+      batch: "radon",
+      organisation: "FunctionUp",
+    },
+    "functionup-radon"
+  );
+  res.setHeader("x-auth-token", token);
+  return res.send({ status: true, token: token });
+};
+
+
+const getUserData = async function (req, res) {
+
+  let userId = req.params.userId;
+  let userDetails = await userModel.findById(userId);
+ 
+
+  return res.send({ status: true, data: userDetails });
+};
+
+const updateUser = async function (req, res) {
+
+  let userId = req.params.userId;
+  let userData = req.body;
+  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData, {new:true});
+  return res.send({ status: true, data: updatedUser });
+};
+
+const deleteUser = async function (req,res) {
+  let userId = req.params.userId
+  let updatedUser = await userModel.findOneAndUpdate({_id: userId}, {isDeleted: true}, {new: true})
+  return res.send({status: true, data: updatedUser})
+}
+
+module.exports.createUser = createUser;
+module.exports.loginUser = loginUser;
+module.exports.getUserData = getUserData;
+module.exports.updateUser = updateUser;
+module.exports.deleteUser = deleteUser;
