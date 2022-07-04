0a50b9b851de8ca3c12bc32280767e324b3705f7
Jai Sharma <102988633+jai84322@users.noreply.github.com>
2022-07-04 15:24:48 +0530

    Update userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema( {
     },
     isDeleted : {
         type : Boolean,
-        default : true
+        default : false
     },
     age: Number,
 }, { timestamps: true });
